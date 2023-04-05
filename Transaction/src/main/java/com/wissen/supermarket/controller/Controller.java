230e13e0ba64b3dfc641e3288fa21840a41b30ee
kunalbandooni <kbandooni1@gmail.com>
2023-04-05 13:28:49 +0530

    microservices completed

--- Transaction/src/main/java/com/wissen/supermarket/controller/Controller.java
+++ Transaction/src/main/java/com/wissen/supermarket/controller/Controller.java
@@ -13,120 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wissen.supermarket.customer.Customer;
-import com.wissen.supermarket.employee.Employee;
-import com.wissen.supermarket.inventory.Inventory;
-import com.wissen.supermarket.services.CustomerService;
-import com.wissen.supermarket.services.EmployeeService;
-import com.wissen.supermarket.services.InventoryService;
-import com.wissen.supermarket.services.TransactionService;
 import com.wissen.supermarket.transaction.Transaction;
+import com.wissen.supermarket.services.TransactionService;
 
 @RestController
 @RequestMapping(value="/api/data/transaction")
 public class Controller {
 
 	@Autowired
-	InventoryService inventoryService;
-
-	@Autowired
-	CustomerService customerService;
-
-	@Autowired
-	EmployeeService employeeService;
-
-	@Autowired
 	TransactionService transactionService;
 
-	// get all products
-	@GetMapping(path="/getInventory")
-	List<Inventory> getAllInventory(){
-		List<Inventory> productList = inventoryService.getAllInventory();
-		if(productList.size() == 0)
-			return productList;		// change this
-		else
-			return productList;
-	}
-	
-	// get product by id
-	@GetMapping(path="/getInventory/{id}")
-	Inventory getProduct(@PathVariable String id) {
-		Optional<Inventory> prodObjOptional = inventoryService.getProductById(id);
-		if(prodObjOptional.isPresent()){
-			Inventory prodObj = prodObjOptional.get();
-			return prodObj;
-		}
-		return new Inventory();
-	}
-	
-	// update Inventory
-	@PutMapping(path="/updateInventory/{check}")
-	String updateInventory(@RequestBody Inventory prodObj, @PathVariable boolean check) {		
-		return inventoryService.updateInventory(prodObj, check);
-	}
-
-	// remove a product
-	@DeleteMapping(path="/removeProduct/{productId}")
-	public String removeProduct(@PathVariable String productId) {
-		return inventoryService.removeProduct(productId);
-	}
-	
-	// create a new product
-	@PostMapping(path="/addProduct")
-	String addProduct(@RequestBody Inventory invObj) {
-		inventoryService.addProduct(invObj);
-		return "Product Added Successfully";
-	}
-
-	// get all employee
-	@GetMapping(path="/getEmployee")
-	List<Employee> getEmployee(){
-		List<Employee> employeeList = employeeService.getAllEmployee();
-		if(employeeList.size() == 0)
-			return employeeList;		// change this
-		else
-			return employeeList;
-	}
-
-	// create a new employee
-	@PostMapping(path="/addEmployee")
-	String addEmployee(@RequestBody Employee cashier) {
-		employeeService.addEmployee(cashier);
-		return "Employee Inserted Successfully";
-	}
-	
-	// remove an Employee
-	@DeleteMapping(path="/removeEmployee/{empId}")
-	public String removeEmployee(@PathVariable String empId) {
-		return employeeService.removeEmployee(empId);
-	}
-	
-	// update Employee Details
-	@PutMapping(path="/updateEmployee")
-	String updateEmployee(@RequestBody Employee cashier) {
-		return employeeService.updateEmployee(cashier);
-	}
-
-	// get all customer
-	@GetMapping(path="/getCustomer")
-	List<Customer> getCustomer(){
-		List<Customer> customerList = customerService.getAllCustomer();
-		if(customerList.size() == 0)
-			return customerList;		// change this
-		else
-			return customerList;
-	}
-
-	// create customer
-	@PostMapping(path="/addCustomer")
-	String addCustomer(@RequestBody Customer customer) {
-		Optional<Customer>checkCustomer=customerService.getCustomerById(customer.getPhoneNumber());
-		if(checkCustomer.isPresent()){
-			return "Already present in database";
-		}
-		customerService.addCustomer(customer);
-		return "Customer Inserted Successfully";
-	}
 	// get all transactions
 	@GetMapping(path="/getTransaction")
 	List<Transaction> getTransaction(){
@@ -155,12 +51,11 @@ public class Controller {
 		
 		return transactionObjOptional;
 	}
+
 	// create transaction
 	@PostMapping(path="/addTransaction")
 	void addTransaction(@RequestBody Transaction transaction) {
 		System.out.println("here");
 		transactionService.addTransaction(transaction);
 	}
-
-
 }
